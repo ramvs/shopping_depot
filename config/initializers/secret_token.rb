15837279b7162b87064e50b74c8a5dcfdc2afcb0
Krystian <krystian.kawa1@gmail.com>
2014-06-17 13:58:56 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ShoppingDepot::Application.config.secret_key_base = '67c39516975211a6cdbbf131f0c98ae59ac6c76fb2dc3b02c85708536bedf390f54f6d375ae54b19b57675ea7f9eb63e643eb48de0fc337cd48353f9c019b68c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ShoppingDepot::Application.config.session_store :cookie_store, key: '_shopping_depot_session'
